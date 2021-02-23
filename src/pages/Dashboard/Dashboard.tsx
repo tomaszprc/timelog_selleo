a82a2c7373042e4fd8f0c4a96a729a05002b1910
Tomasz <t.prochowicz@selleo.com>
2021-02-23 10:04:09 +0100

    #3 Set prettier and fixed code format, created routing for components

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+interface DashboardProps {}
+
+const Dashboard: FunctionComponent<DashboardProps> = ({}) => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
---
+++ src/pages/Projects/Projects.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+interface ProjectsProps {}
+
+const Projects: FunctionComponent<ProjectsProps> = ({}) => {
+  return <div>Projects</div>;
+};
+
+export default Projects;
